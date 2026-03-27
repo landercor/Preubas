506bd7b6e28887d04f6b15bf8d9187a175e6d4aa
landercor <asprillafabian034@gmail.com>
2026-03-26 22:30:47 -0500

    organizando archivos

---
+++ continuacion.java
@@ -0,0 +1,16 @@
+
+class continuacion {
+
+    public static void main(String[] args) {
+        String nombre = "andres";
+        String ciudad = "bogota";
+        String edad = "19";
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Ciudad: " + ciudad);
+        System.out.println("Edad: " + edad);
+        if (edad.equals("19")) {
+            System.out.println("tienes x meses de edad");
+        }
+
+    }
+}
---
+++ edadR.java
@@ -0,0 +1,24 @@
+
+class edadR {
+
+    int edadR = 17;
+    boolean tieneCuenta = true;
+    boolean estaBloqueado = false;
+
+    public static void main(String[] args) {
+        edadR persona = new edadR();
+
+        if (persona.estaBloqueado) {
+            System.out.println("La cuenta esta bloqueada");
+
+        } else if (persona.edadR < 18) {
+            System.out.println("El usuario es menor de edad");
+
+        } else if (persona.tieneCuenta) {
+            System.out.println("Usuario logeado");
+
+        } else {
+            System.out.println("Usuario no registrado");
+        }
+    }
+}
